b5a221ab42b58a55fdbb489eb11885d157919bae
Arthur <paracidex@gmail.com>
2024-02-19 00:47:50 -0600

    fix a bug: checkbox not work

--- src/components/shared/TlButton.tsx
+++ src/components/shared/TlButton.tsx
@@ -4,6 +4,7 @@ import {persistStore, setTargetLang} from "@/store/store";
 import {Checkbox, CheckboxGroup} from "@nextui-org/checkbox";
 import LangModal from "./LangModal";
 import {useTranslations} from "next-intl";
+import {useSnapshot} from "valtio";
 
 type Props = {
   langArr: Array<[string, string]>
@@ -11,15 +12,18 @@ type Props = {
 
 export default function OlButton({langArr}: Readonly<Props>) {
   const componentsT = useTranslations("components")
+  const persistSnap = useSnapshot(persistStore);
 
   return (
     <LangModal title={componentsT('buttons.targetLang')}>
       <CheckboxGroup color="secondary"
                      orientation="horizontal"
-                     value={persistStore.targetLang}
-                     onValueChange={setTargetLang}>
+                     value={[...persistSnap.targetLang]}
+                     onValueChange={setTargetLang}
+      >
         {langArr.map(([k, v]) => (
-          <Checkbox size="sm" className="max-w-none md:w-48 w-44" key={k} id={k} value={k} color="secondary">
+          <Checkbox size="sm" className="max-w-none md:w-48 w-44" key={k}
+                    id={k} value={k} color="secondary">
             {v}
           </Checkbox>
         ))}
