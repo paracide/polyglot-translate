19f5247c596477dc884ba04ee8afdf360ea39791
Arthur <paracidex@gmail.com>
2024-03-11 23:51:32 -0500

    fix persistStore bug

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 import ResultCard from '@/components/shared/ResultCard';
-import React from "react";
-import {persistStore} from "@/store/store";
+import React, {useEffect} from "react";
+import {persistStore, STATE_KEY} from "@/store/store";
 import {useSnapshot} from "valtio";
 import SearchCard from "@/components/shared/SearchCard";
 
 export default function IndexPage() {
   const storageSnap = useSnapshot(persistStore);
+  //only read the constant from localstorage when load the page or refresh
+  useEffect(() => {
+    const storedState = localStorage.getItem(STATE_KEY);
+    if (storedState) {
+      Object.assign(persistStore, JSON.parse(storedState));
+    }
+  }, [])
+
 
   return (
     <main className="wrapper min-h-screen grid grid-cols-1 md:grid-cols-4 md:grid-rows-4 gap-3">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 'use client'
 import {redirect} from 'next/navigation';
-import {useEffect} from "react";
-import {persistStore, STATE_KEY} from "@/store/store";
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
-  //only read the constant from localstorage when load the page or refresh
-  useEffect(() => {
-    const storedState = localStorage.getItem(STATE_KEY);
-    if (storedState) {
-      Object.assign(persistStore, JSON.parse(storedState));
-    }
-  }, [])
 
   redirect('/en');
 }
